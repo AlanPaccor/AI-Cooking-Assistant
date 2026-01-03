a53a13e9921370f611cfef93ef63130e45960bea
AlanPaccor <alanpaccor@gmail.com>
2026-01-03 01:19:32 -0500

    Added Tailwind styles, and bones

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+import CookingButton from "../components/CookingButton";
+
+export default function Home() {
+  return (
+    <View className="flex-1 items-center justify-center px-5 bg-white">
+      <Text className="text-3xl font-bold mb-2">👨‍🍳 AI Cooking Assistant</Text>
+      <Text className="text-base text-center mb-8 text-gray-600">
+        I’ll guide you step-by-step while you cook.
+      </Text>
+
+      <CookingButton />
+    </View>
+  );
+}
